0c514a428f772da51b3acb21105ae5cdc41f25db
or-haklay <or.haklay@gmail.com>
2025-08-20 01:10:37 +0300

    Update userModel.js

--- models/userModel.js
+++ models/userModel.js
@@ -8,7 +8,7 @@ const userSchema = new mongoose.Schema(
     email: { type: String, required: true, unique: true },
     phone: { type: String },
 
-    password: { type: String },
+    password: { type: String, select: false },
 
     googleId: { type: String },
     facebookId: { type: String },
